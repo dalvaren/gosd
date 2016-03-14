9a30b19a28bd27f02b29436edf58073e0ad07d4e
Daniel Campos <danielalvarengacampos@gmail.com>
2016-03-14 16:09:36 -0300

    basic working for service discovery

--- main.go
+++ main.go
@@ -7,13 +7,14 @@ package main
 import "os"
 import "fmt"
 import "time"
+import "strings"
 import "strconv"
 import redis "gopkg.in/redis.v3"
 
 type Driver interface {
-  Start()
-  Set()
+  Start(name, url string) string
   Get()
+  Delete(currentName string)
 }
 
 
@@ -23,19 +24,55 @@ type ServiceCacheEntry struct {
 }
 
 type Updater struct {
+  Name          string
+  State         string // expired or updated
   TTL           time.Time
-  State         string
   Driver        Driver
-  ServiceCache  []ServiceCacheEntry
+  ServiceCacheEntries  []ServiceCacheEntry
+}
+
+type ServiceMap struct {
+  Index int
+  URLs  []string
 }
 
 var RedisClient *redis.Client
+var ServiceMaps map[string]ServiceMap
+var ServiceUpdater Updater
 
 func main() {
   fmt.Println("Starting GOSD client")
-  name := "service-2"
-  url := "http://localhost:8881"
 
+  // start
+  currentName := Start("service-2", "http://localhost:8881")
+  // fmt.Println(currentName)
+
+  // get
+  Get()
+
+  // finish
+  Finish(currentName)
+
+  // IterateServiceRoute
+  fmt.Println(IterateServiceRoute("service-2"))
+  fmt.Println(IterateServiceRoute("service-2"))
+  fmt.Println(IterateServiceRoute("service-2"))
+}
+
+func IterateServiceRoute(serviceBaseName string) string {
+  return getNextURLForService(serviceBaseName)
+}
+
+func Finish(currentName string) {
+  // delete
+  Delete(currentName)
+}
+
+func Delete(currentName string) {
+  RedisClient.HDel("gosd", currentName)
+}
+
+func Start(name, url string) string {
   // start
   redisDB,_ := strconv.Atoi(os.Getenv("RedisDB"))
   RedisClient = redis.NewClient(&redis.Options{
@@ -50,14 +87,73 @@ func main() {
 
   // set
   currentName := registerService(name, url)
-  fmt.Println(currentName)
-
-  // get
-  val := tryRefreshForNTimes(3)
-  for key,value := range val {
-    fmt.Println(key,value)
+  ServiceUpdater = Updater{
+    Name: currentName,
+    State: "expired",
+    TTL:    time.Now(),
   }
 
+  return currentName
+}
+
+func Get() {
+  val := tryRefreshForNTimes(3)
+  for key,value := range val {
+    // populate Updater
+    serviceCacheEntry := ServiceCacheEntry{
+      Name: key,
+      URL: value,
+    }
+    ServiceUpdater.ServiceCacheEntries = append(ServiceUpdater.ServiceCacheEntries, serviceCacheEntry)
+  }
+  recalculateServiceMaps(ServiceUpdater)
+}
+
+func getNextURLForService(baseName string) string {
+  serviceMaps, exists := ServiceMaps[baseName]
+  if !exists {
+    return ""
+  }
+  if serviceMaps.Index >= len(ServiceMaps[baseName].URLs) {
+    serviceMaps.Index = 0
+  }
+  serviceMaps.Index++
+  ServiceMaps[baseName] = serviceMaps
+  return ServiceMaps[baseName].URLs[(serviceMaps.Index - 1)]
+}
+
+func recalculateServiceMaps(updater Updater) {
+  if len(updater.ServiceCacheEntries) == 0 {
+    return
+  }
+  serviceMaps := map[string]ServiceMap{}
+  for _,serviceCacheEntry := range updater.ServiceCacheEntries {
+    serviceMap := getServiceMap(getServiceBaseName(serviceCacheEntry.Name), serviceMaps)
+    serviceMap.URLs = append(serviceMap.URLs, serviceCacheEntry.URL)
+    serviceMaps[getServiceBaseName(serviceCacheEntry.Name)] = serviceMap
+  }
+
+  ServiceMaps = serviceMaps
+}
+
+
+func getServiceMap(baseName string, serviceMaps map[string]ServiceMap) ServiceMap {
+  for key,serviceMap := range serviceMaps {
+    if key == baseName {
+      return serviceMap
+    }
+  }
+  return ServiceMap{
+    Index: 0,
+    URLs:  []string{},
+  }
+}
+
+func getServiceBaseName(name string) string {
+  if index := strings.LastIndex(name, "-"); index > -1 {
+    return name[0:index]
+  }
+  return name
 }
 
 func getNextServiceURL(name string) string {
